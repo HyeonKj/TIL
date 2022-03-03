8bd1ace73bca82eac631214ac7417e6ec33ebed3
HyeonKj <8.8.naine@gmail.com>
2022-03-03 14:46:08 +0900

    Algorithm_BOJ_2953

--- Algorithm/BOJ_list.py
+++ Algorithm/BOJ_list.py
@@ -1,4 +1,16 @@
-#no.11720
+""" #no.11720
 number = int(input())
 
-print(sum(map(int, input())))
+print(sum(map(int, input()))) """
+
+#no. 2750
+number = int(input())
+num = []
+
+for i in range(number):
+    num.append(int(input()))
+
+num.sort()
+
+for i in num:
+    print(i)
