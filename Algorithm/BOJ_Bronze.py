89ca7e82a04917825feb663f72974cc5dad6b2c4
HyeonKj <8.8.naine@gmail.com>
2022-03-04 11:48:47 +0900

    Algorithm_BOJ_Bronze_3

--- Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Bronze.py
@@ -6,7 +6,7 @@ for i in range(2):
         print(list_a[1][j] * list_b[i][j], end = " ")
     print() """
 
- # 비슷한 다른 문제1 
+"""  # 비슷한 다른 문제1 
 list_a = [list(map(int, input().split())) for i in range(2)]
 list_b = [list(map(int, input().split())) for i in range(2)]
 
@@ -31,16 +31,24 @@ list_c = [[list_a[i][j] * list_b[i][j] for j in range(3)] for i in range(2)]
 for i in range(2):
     for j in range(3):
         print(list_c[i][j], end=" ")
-    print()
+    print() """
 
 
 # no.1100 하얀 칸 
+ans=0
+for i in range(8):
+     ans+=input()[i%2::2].count('F')
+print(ans)
 
-chess = [input() for i in range(8)]
-num = 0
-
-for j in range(8):
-    for x in range(8):
-        if y % 2 == x % 2 and chess[j][x] == 'F':
-            num + =1
-print(num)
+#하얀 칸 길게 풀어 쓴 코드 
+chess = 0
+for i in range(8):
+    table = str(input())
+    for j in range(8):
+        if i%2 == 0 and j%2 == 0:
+            if table[j] == 'F':
+                chess+=1
+        elif i%2!=0 and j%2!=0:
+            if table[j]=='F':
+                chess+=1
+print(chess)
