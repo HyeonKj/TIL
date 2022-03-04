5bb78e7cbdd5043e482d110aba96452673e318c7
HyeonKj <8.8.naine@gmail.com>
2022-03-04 15:46:11 +0900

    Algorithm_dict_base

--- Algorithm/JUNGOL_dict.py
+++ Algorithm/JUNGOL_dict.py
@@ -1,4 +1,4 @@
-# 딕셔너리 기본 문제 
+""" # 딕셔너리 기본 문제 
 animations = {
     "Pockemon" : "Pickachu",
     "Digimon" : "Agumon",
@@ -11,5 +11,41 @@ word = input()
 # else:
 #     print("I don't know")
 
-print(animations.get(word, "I don't know"))
+print(animations.get(word, "I don't know")) """
 
+# 딕셔너리 기본 문제 2
+# num = int(input())
+# nat_cap = {}
+
+# for i in range(num):
+#     word = input().split()
+#     nat_cap[word[0]]= word[1]
+
+# word = input()
+# print(nat_cap.get(word,'Unknown Country'))
+
+#### 다른 방법 #####
+# num = int(input())
+
+# dict = {}
+
+# for i in range(num):
+#     country = input().split()
+#     dict[country[0]] = country[1]
+
+# country = input()
+# print(dict.get(country, "Unknown Country"))
+
+# dictionary 
+foul = list(input().split())
+dic = {}
+
+for name in set(foul):
+    dic[name] = foul.count(name)
+
+min_foul = min(dic.values())
+
+for name in dic:
+    if dic[name] == min_foul:
+        print(name)
+print(min_foul)
