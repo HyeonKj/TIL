6c81ef99218c038b7dbfa27e46221c03f3ec8f44
HyeonKj <8.8.naine@gmail.com>
2022-03-05 11:11:28 +0900

    Algorithm_Bronze_algorithm2

--- Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Bronze.py
@@ -126,3 +126,15 @@ for i, char in enumerate(first):
         pattern += char
 
 print(pattern)
+
+# 더 간결한 풀이
+
+n = int(input())
+files = [input() for _ in range(n)]
+pattern = ""
+
+for columns in zip(*files):
+    pattern += "?" if columns.count(columns[0]) < n else columns[0]
+
+print(pattern)
+
