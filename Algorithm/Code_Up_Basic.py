a77c3cbc6590f61322ac20dd7accd260687e6341
HyeonKj <8.8.naine@gmail.com>
2022-03-06 20:17:53 +0900

    Code_up_basic_3

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -3,7 +3,13 @@
 # print(a)
 
 # 2. @@@@@@@@@@@@@@@@@@@
-b = float(input())
-print(b)
+# b = float(input())
+# print(b)
 
 # 3. ################
+a = input() 
+b = input()
+print(a)
+print(b)
+
+# 4. ##################
