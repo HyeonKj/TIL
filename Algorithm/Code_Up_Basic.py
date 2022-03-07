b59ebd2394a2e8f7c3bd752e153ddd7399e317b5
HyeonKj <8.8.naine@gmail.com>
2022-03-07 23:47:04 +0900

    Code_Up_Basic_18

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -76,8 +76,14 @@ print(b) """
 # print(c)
 
 # 17. @@@@@@@@@@@@@
-n1 = float(input())
-n2 = float(input())
-print(n1 + n2)
+# n1 = float(input())
+# n2 = float(input())
+# print(n1 + n2)
 
 #18. #####################3
+
+a = input()
+n = int(a)            #입력된 a를 10진수 값으로 변환해 변수 n에 저장
+print('%x'% n)  #n에 저장되어있는 값을 16진수(hexadecimal) 소문자 형태 문자열로 출력
+
+# 19. ###############
