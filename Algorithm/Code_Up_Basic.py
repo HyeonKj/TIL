9bacac7f63b590895728718fc7cba84b8a75f14c
HyeonKj <8.8.naine@gmail.com>
2022-03-12 23:34:38 +0900

    CODE_UP_45

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -195,3 +195,13 @@ print(-n)
 # 수 - 수는 차(subtraction)가 계산된다.
 
 
+
+# 실수 2개(f1, f2)를 입력받아 곱을 출력하는 프로그램을 작성해보자.
+
+# 예시
+# ...
+# m = float(f1) * float(f2)
+# print(m)
+
+# 참고
+# 수 * 수는 곱(multiplication)이 계산된다.
