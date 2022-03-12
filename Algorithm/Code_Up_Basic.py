edc526b4375cf532a3990dc2dd9ffe46b1c4ad7d
HyeonKj <8.8.naine@gmail.com>
2022-03-12 23:43:11 +0900

    Codeup_basic_hard_blog6095

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -431,7 +431,43 @@ for i in range(a, a + n - 1):
 print(total)
 
 
+#6091
+a, b, c = map(int, input().split())
+d = 1
+while d % a != 0 or d % b != 0 or d % c != 0:
+    d += 1
+print(d)
+#6092
+from random import randint
+n = int(input())
+temp = [0] * 23
+nums = input().split()
+for i in nums:
+    temp[int(i)-1] += 1
+for i in temp:
+    print(i, end=' ')
+#6093
+n = int(input())
+nums = input().split()
+nums.reverse()
+for i in nums:
+    print(int(i), end=' ')
+#6094
+n = int(input())
+nums = map(int, input().split())
+print(min(nums))
+#6095
+li = [[0 for i in range(19)] for j in range(19)]
+n = int(input())
+for i in range(n):
+    x, y = map(int, input().split())
+    if(li[x-1][y-1] != 1):
+        li[x-1][y-1] = 1
+for i in li:
+	print(' '.join(map(str, i)))
 
+
+  
 
 
 
