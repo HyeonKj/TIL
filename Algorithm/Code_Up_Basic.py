f4ab4c1213030eacb5567471e05ce1e49d851b19
HyeonKj <8.8.naine@gmail.com>
2022-03-16 00:55:07 +0900

    Algorithm_codeup_6076

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -761,6 +761,15 @@ while(count <= ord(a)-97):
     print(chr(97+count), end=' ')
     count += 1
 
+#6075
+n = int(input())
+for i in range(n + 1):
+    print(i)
+#6076
+n = int(input())
+for i in range(n + 1):
+    print(i)
+
 
 
 
