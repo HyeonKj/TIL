664b2d52a2cb6a9d31dbd677bb0707493ce1faa9
HyeonKj <8.8.naine@gmail.com>
2022-03-16 00:53:58 +0900

    Algorithm_codeup_6070

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -710,6 +710,31 @@ elif(score >= 40):
 elif(score >= 0):
     print('D')
 
+#6069
+grade = input()
+if(grade == 'A'):
+    print('best!!!')
+elif(grade == 'B'):
+    print('good!!')
+elif(grade == 'C'):
+    print('run!')
+elif(grade == 'D'):
+    print('slowly~')
+else:
+    print('what?')
+#6070
+month = int(input())
+season = ''
+if(month in [12, 1, 2]):
+    season = 'winter'
+elif(month in [3, 4, 5]):
+    season = 'spring'
+elif(month in [6, 7, 8]):
+    season = 'summer'
+else:
+    season = 'fall'
+print(season)
+
 
 
 
