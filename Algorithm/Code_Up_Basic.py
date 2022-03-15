3d2e4051b5cbb63240f698f999f393a0fe82f5cf
HyeonKj <8.8.naine@gmail.com>
2022-03-16 00:54:34 +0900

    Algorithm_codeup_6074

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -749,6 +749,18 @@ while(num > 0):
     print(num)
     num -= 1
 
+#6073
+num = int(input())
+while(num > 0):
+    num -= 1
+    print(num)
+#6074
+a = input()
+count = 0
+while(count <= ord(a)-97):
+    print(chr(97+count), end=' ')
+    count += 1
+
 
 
 
