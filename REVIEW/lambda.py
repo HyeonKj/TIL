bac8b5b664b47340264879e900e2452d14c72a57
HyeonKj <8.8.naine@gmail.com>
2022-03-15 18:00:20 +0900

    lambda_py_03

--- REVIEW/lambda.py
+++ REVIEW/lambda.py
@@ -4,4 +4,14 @@ map(lambda x: x ** 2, range(5))             # 파이썬 2
 list(map(lambda x: x ** 2, range(5)))     # 파이썬 2 및 파이썬 3
 [0, 1, 4, 9, 16]
 
-lambda 매개변수 : 표현식
+# lambda 매개변수 : 표현식
+
+# >>> def hap(x, y):
+# ...   return x + y
+# ...
+# >>> hap(10, 20)
+# 30
+
+# 램다 함수식 표현
+# >>> (lambda x,y: x + y)(10, 20)
+# 30
