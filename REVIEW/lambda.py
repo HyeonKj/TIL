0b2c34c259038acfadd933353c04a0a21a9b0af3
HyeonKj <8.8.naine@gmail.com>
2022-03-15 17:59:15 +0900

    lambda_py

---
+++ REVIEW/lambda.py
@@ -0,0 +1,7 @@
+# map(함수, 리스트)
+map(lambda x: x ** 2, range(5))             # 파이썬 2
+[0, 1, 4, 9, 16]  
+list(map(lambda x: x ** 2, range(5)))     # 파이썬 2 및 파이썬 3
+[0, 1, 4, 9, 16]
+
+lambda 매개변수 : 표현식
