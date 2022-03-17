4a6599f0748075fac9a357538b2ff975b50bace0
HyeonKj <8.8.naine@gmail.com>
2022-03-17 21:28:28 +0900

    Code_up_s6078

--- Algorithm/Code_Up_Basic.py
+++ Algorithm/Code_Up_Basic.py
@@ -770,6 +770,19 @@ n = int(input())
 for i in range(n + 1):
     print(i)
 
+#6077
+n = int(input())
+s = 0
+for i in range(n + 1):
+    if(i % 2 == 0):
+        s += i
+print(s)
+#6078
+c = ''
+while(c != 'q'):
+    c = input()
+    print(c)
 
+    
 
 
