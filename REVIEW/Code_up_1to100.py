83559701da88ce7db3a40b9614cca1bfde1577c4
HyeonKj <8.8.naine@gmail.com>
2022-03-18 14:46:18 +0900

    review_basic6001

---
+++ REVIEW/Code_up_1to100.py
@@ -0,0 +1,20 @@
+# code _up 파이썬 기초 문제 100선 
+
+# #basic output
+#6001
+print('Hello')
+#6002
+print('Hello World')
+#6003
+print('Hello')
+print('World')
+#6004
+print("'Hello'")
+#6005
+print('''"Hello World"''')
+#6006
+print('"!@#$%^&*()\'')
+#6007
+print('''"C:\\Download\\\'hello'.py"''')
+#6008
+print('''print("Hello\\nWorld")''')
