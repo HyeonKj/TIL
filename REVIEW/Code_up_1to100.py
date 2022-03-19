5664aebc53671f7d293cbad3871e1922e5a35b6d
HyeonKj <8.8.naine@gmail.com>
2022-03-19 15:16:44 +0900

    0319basic_6038

--- REVIEW/Code_up_1to100.py
+++ REVIEW/Code_up_1to100.py
@@ -124,3 +124,11 @@ for i in range(int(s[1])):
 # s = input().split()
 # print(s[0] * int(s[1]))
 
+#6037
+count = int(input())
+s = input()
+print(s * count)
+#6038
+a, b = input().split()
+print(int(a) ** int(b))
+
