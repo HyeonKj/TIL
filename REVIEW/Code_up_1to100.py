c5c8bffea38c614fc8973e9fe7335760c769f993
HyeonKj <8.8.naine@gmail.com>
2022-03-20 21:33:51 +0900

    Review_6066

--- REVIEW/Code_up_1to100.py
+++ REVIEW/Code_up_1to100.py
@@ -249,3 +249,11 @@ for i in data:
     if(int(i) % 2 == 0):
         print(int(i))
 
+#6066
+data = input().split()
+for i in data:
+    if(int(i) % 2 == 0):
+        print('even')
+    else:
+        print('odd')
+
