d5610051c5b4f818d3fca53b4c6c6869698199f9
HyeonKj <8.8.naine@gmail.com>
2022-03-20 21:34:11 +0900

    Review_6067

--- REVIEW/Code_up_1to100.py
+++ REVIEW/Code_up_1to100.py
@@ -257,3 +257,14 @@ for i in data:
     else:
         print('odd')
 
+#6067
+data = int(input())
+if(data % 2 == 0 and data < 0):
+    print('A')
+elif(data % 2 != 0 and data < 0):
+    print('B')
+elif(data % 2 == 0 and data > 0):
+    print('C')
+elif(data % 2 != 0 and data > 0):
+    print('D')
+
