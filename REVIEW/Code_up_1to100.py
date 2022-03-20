0a1b36a3478780a0dadb2ca33046d751e6b44f0c
HyeonKj <8.8.naine@gmail.com>
2022-03-20 21:35:12 +0900

    Review_6070

--- REVIEW/Code_up_1to100.py
+++ REVIEW/Code_up_1to100.py
@@ -292,3 +292,16 @@ elif(grade == 'D'):
 else:
     print('what?')
 
+#6070
+month = int(input())
+season = ''
+if(month in [12, 1, 2]):
+    season = 'winter'
+elif(month in [3, 4, 5]):
+    season = 'spring'
+elif(month in [6, 7, 8]):
+    season = 'summer'
+else:
+    season = 'fall'
+print(season)
+
