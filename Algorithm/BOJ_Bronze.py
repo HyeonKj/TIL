2f390d93052f1f6d2b5ee9443731bcc77c7a7b54
HyeonKj <8.8.naine@gmail.com>
2022-03-27 21:55:50 +0900

    Algorithm_bronze0327_03

--- Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Bronze.py
@@ -242,3 +242,11 @@ num = tuple(map(int, input().split()))
 while num != (0, 0):
     a = num[0]
 
+# 1712
+a, b, c = map(int, input().rstrip().split())
+
+if c - b <= 0:
+    print(-1)
+else:
+    print(a//(c-b) + 1)
+
