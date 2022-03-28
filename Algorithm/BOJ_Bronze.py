e9a35e3923b2482316520e8eaf627c342e027386
HyeonKj <8.8.naine@gmail.com>
2022-03-28 22:51:34 +0900

    Algorithm_bronze_2156

--- Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Bronze.py
@@ -344,3 +344,25 @@ for i in range(t):
 
         print("%d %d" % (d[n][0], d[n][1]))
 
+#2156
+n = int(input())
+
+wine = []
+
+for i in range(n):
+    wine.append(int(input()))
+
+d = [0]*n
+
+d[0] = wine[0]
+if n > 1:
+    d[1] = wine[0]+wine[1]
+
+if n > 2:
+    d[2] = max(wine[2]+wine[1], wine[2]+wine[0], d[1])
+
+for i in range(3, n):
+    d[i] = max(d[i-1], d[i-3]+wine[i-1]+wine[i], d[i-2]+wine[i])
+
+print(d[n-1])
+
