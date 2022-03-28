111e4bd99a259576e293ddf50a1adf8eb1d2c76d
HyeonKj <8.8.naine@gmail.com>
2022-03-28 22:55:49 +0900

    Algorithm_bronze_1932

--- Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Bronze.py
@@ -482,3 +482,22 @@ for i in range(1, n):
 
 print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
 
+#1932
+n = int(input())
+
+arr = []
+dp = [[0]*n for _ in range(n)]
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+dp[0][0] = arr[0][0]
+
+for i in range(1, n):
+    for j in range(0, i+1):
+        if j == 0:
+            dp[i][j] = dp[i-1][j] + arr[i][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
+
+print(max(dp[n-1]))
+
