95e79d3dded2715365d672738962e57a818eeeed
HyeonKj <8.8.naine@gmail.com>
2022-03-30 22:27:39 +0900

    algorithm_1032

--- Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -533,5 +533,16 @@ for _ in range(T):
         else:
             print((a**b) % 10 % 10 % 10)
 
-# 
+# 1032
+n = int(input())
+a = list(input())
+a_len = len(a)
+for i in range(n - 1):
+    b = list(input())
+    for j in range(a_len):
+        if a[j] != b[j]:
+            a[j] = '?'
+print(''.join(a))
+
+
 
