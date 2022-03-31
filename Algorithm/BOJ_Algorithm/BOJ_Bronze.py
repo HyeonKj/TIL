360479191c6962d22e565ea4e9cab2c88c529bfa
HyeonKj <8.8.naine@gmail.com>
2022-03-31 21:58:15 +0900

    0331_algorithm_4

--- Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -620,5 +620,19 @@ if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
 else:
     print(0)
 
-#
+#14681
+x= int(input())
+y= int(input())
+
+if x > 0 and y > 0 :	# x,y: 양수
+    print('1')
+elif x < 0 and y > 0 :	# x:음수, y:양수
+    print('2')
+elif x < 0 and y < 0 :	# x,y: 음수
+    print('3')
+else:
+    print('4')
+
+
+
 
