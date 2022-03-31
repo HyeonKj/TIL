9351163f72b4de0cacdedb0fd66b6226845019ef
HyeonKj <8.8.naine@gmail.com>
2022-03-31 21:58:51 +0900

    0331_algorithm_5

--- Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -633,6 +633,19 @@ elif x < 0 and y < 0 :	# x,y: 음수
 else:
     print('4')
 
+# 2884
+H, M = map(int, input().split())
 
+if M < 45 :	# 분단위가 45분보다 작을 때 
+    if H == 0 :	# 0 시이면
+        H = 23
+        M += 60
+    else :	# 0시가 아니면 (0시보다 크면)
+        H -= 1	
+        M += 60
+        
+print(H, M-45)	
+
+#
 
 
