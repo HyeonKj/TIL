f6356737274d388932d37b29e44df1e17be2c01d
HyeonKj <8.8.naine@gmail.com>
2022-04-01 21:39:25 +0900

    dacon_abaone_

---
+++ DACON/abalone_age_predict.py
@@ -0,0 +1,10 @@
+# Basic Library
+import os
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+# Preprocessing
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, QuantileTransformer
+
