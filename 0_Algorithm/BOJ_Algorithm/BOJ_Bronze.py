c14072e0817d9ecaf0bbb9b08933db6ed9e61966
HyeonKj <8.8.naine@gmail.com>
2022-04-11 01:11:39 +0900

    algorithm_5086

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -900,3 +900,17 @@ for i in range(1, len(nums)) :
         
 for n in nums : 
     print(n)
+
+
+# 5086 
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    if b % a == 0:
+        print('factor')
+    elif a % b == 0:
+        print('multiple')
+    else:
+        print('neither')
+
