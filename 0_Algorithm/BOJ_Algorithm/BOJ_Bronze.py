6dcd08fbc2c2cc82ae30cbae4b9954467243fe30
HyeonKj <8.8.naine@gmail.com>
2022-04-10 20:11:24 +0900

    BOJ_bronze_10870

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -831,3 +831,12 @@ if n > 0:
         result *= i
 print(result)
 
+#10870
+n = int(input())
+
+fibonacci = [0, 1]
+for i in range(2, n+1):
+    num = fibonacci[i-1] + fibonacci[i-2]
+    fibonacci.append(num)
+print(fibonacci[n])
+
