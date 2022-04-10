f2ec80be199a2a78d7e480c9deea9b9679c10872
HyeonKj <8.8.naine@gmail.com>
2022-04-11 01:10:44 +0900

    algorithm_2750

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -867,4 +867,36 @@ for i in range(1, n+1):   # 해당 분해합의 생성자 찾기
     if i == n:  # 생성자 i와 입력값이 같다는 것은 생성자가 없다는 뜻
         print(0)
 
-#
+# 2750 수 정렬 버블정렬
+N = int(input())
+
+numbers = []
+
+for _ in range(N) : 
+    numbers.append(int(input()))
+
+# Bubble Sort
+for i in range(len(numbers)) : 
+    for j in range(len(numbers)) : 
+        if numbers[i] < numbers[j] : 
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            
+for n in numbers : 
+    print(n)
+
+# 삽입 정렬
+N = int(input())
+nums = []
+
+for _ in range(N) : 
+    nums.append(int(input()))
+
+# Insert Sort
+for i in range(1, len(nums)) :
+    while (i>0) & (nums[i] < nums[i-1]) :
+        nums[i], nums[i-1] = nums[i-1], nums[i]
+        
+        i -= 1
+        
+for n in nums : 
+    print(n)
