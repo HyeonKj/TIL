e6e6da7606dddd97b6ec763cf5e5522f8ff4fe60
HyeonKj <8.8.naine@gmail.com>
2022-04-12 22:46:00 +0900

    bronze1110

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1005,3 +1005,17 @@ t = color.index(input())
 r = int(str(f) + str(s)) * (10 ** t)
 print(r)
 
+#1110
+input_num = int(input())
+
+num = input_num  # num 변수에 input_num을 지정
+cnt = 0
+while True:
+    sum_num = (num // 10) + (num % 10)  # 각 자릿수를 더한수
+    new_num = ((num % 10) * 10) + (sum_num % 10)  # 새로 만들어지는 수
+    cnt += 1  # 사이클 카운트
+    if new_num == input_num :
+        break
+    num = new_num  # num 변수에 last_num을 지정 
+print(cnt)
+
