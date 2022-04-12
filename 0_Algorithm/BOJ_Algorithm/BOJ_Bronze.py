a3bbc1593b6a71b0cff290dc526485625e971df0
HyeonKj <8.8.naine@gmail.com>
2022-04-12 22:43:45 +0900

    bronze1009

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -959,3 +959,30 @@ print(round(a/b,9))
 
 a, b = map(int , input().split()) 
 print(a/b)
+
+# 1009 분산처리
+import sys 
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+	a,b = map(int,input().split())
+	aa=a%10
+
+	if aa == 0: # 패턴 1
+		print(10)
+	elif aa in [1,5,6]: 
+		print(aa)
+	elif aa in [4,9]: #패턴 2
+		bb=b%2
+		if bb == 0:
+			print(aa*aa%10)
+		else:
+			print(aa)
+	else: #패턴 4
+		bb=b%4  
+		if bb ==0:
+			print(aa**4%10)
+		else:
+			print(aa**bb%10)
+
