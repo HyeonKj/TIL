75e71c960b197fae01ee549a710a88a8403bb6d0
HyeonKj <8.8.naine@gmail.com>
2022-04-13 22:51:22 +0900

    algorithm_BOJ_1145

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1019,3 +1019,17 @@ while True:
     num = new_num  # num 변수에 last_num을 지정 
 print(cnt)
 
+# 1145 적어도 대부분의 배수 
+a = list(map(int, input().split()))
+n = min(a)
+while True:
+    cnt = 0
+    for i in range(5):
+        if n % a[i] == 0:
+            cnt += 1
+    if cnt > 2:
+        print(n)
+        break
+    n += 1
+
+# 
