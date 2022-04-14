0613579c1cd53e6427d6740decf0ba2d10be51bb
HyeonKj <8.8.naine@gmail.com>
2022-04-14 13:24:12 +0900

    Algorithm_BOJ_1173

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1075,4 +1075,19 @@ else:
         print(i, end='')
 
 
-# 
+# 1173
+N, m, M, T, R = map(int, input().split())
+cnt = t = 0
+now = m
+while cnt < N:
+    if m+T > M:
+        break
+    if now + T <= M:
+        now += T
+        cnt += 1
+    else:
+        now = max(now-R, m)
+    t += 1
+print(t if cnt == N else -1)
+
+
