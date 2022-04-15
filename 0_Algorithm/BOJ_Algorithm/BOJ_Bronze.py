b4689bbed912e8048cad829bce79f4cd6f35e084
HyeonKj <8.8.naine@gmail.com>
2022-04-15 11:02:11 +0900

    algorithm_bronze_1264

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1179,4 +1179,14 @@ while True:
     else:
         print('no')
 
-# 
+# 1264
+while 1:
+    s = input()
+    if s == '#':
+        break
+    cnt = 0
+    for c in s:
+        if c in 'aeiouAEIOU':
+            cnt += 1
+    print(cnt)
+
