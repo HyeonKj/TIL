e16ce011df6052b376f38936a76c1cfebb3148fe
HyeonKj <8.8.naine@gmail.com>
2022-04-15 10:58:47 +0900

    algorithm_bronze_1236

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1127,3 +1127,23 @@ for i in range(1, s1+1):
             li[i+j+k] += 1
 print(li.index(max(li)))
 
+#1236 성 지키기
+n, m = map(int,input().split())
+board = []
+
+for _ in range(n):
+    board.append(input())
+
+a, b = 0, 0
+
+for i in range(n):
+    if "X" not in board[i]:
+        a += 1
+
+for j in range(m):
+    if "X" not in [board[i][j] for i in range(n)]:
+        b += 1
+
+print(max(a ,b))
+
+# 
