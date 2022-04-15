251e17fee843daef691ea736f9553c2e74369ee1
HyeonKj <8.8.naine@gmail.com>
2022-04-16 00:31:34 +0900

    Algorithm_bronze_1356

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1277,4 +1277,21 @@ for i in s:
         sum += i // a + 1
 print(sum * a)
 
-# 
+# 1356
+n = input()
+n_len = len(n)
+true = 0
+for i in range(n_len - 1):
+    left = 1
+    right = 1
+    for j in range(i + 1):
+        left *= int(n[j])
+    for k in range(i + 1, n_len):
+        right *= int(n[k])
+    if left == right:
+        print("YES")
+        true = 1
+        break
+if true == 0:
+    print("NO")
+
