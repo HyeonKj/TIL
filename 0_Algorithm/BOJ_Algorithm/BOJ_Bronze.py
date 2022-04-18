828a4aa62dddc9677b1485d80378ed6a32b69431
HyeonKj <8.8.naine@gmail.com>
2022-04-18 15:50:37 +0900

    algorithm_bronze_1362

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1300,4 +1300,36 @@ x, y = map(str, input().split())
 s = str(int(x[::-1]) + int(y[::-1]))
 print(int(s[::-1]))
 
+# 1362
+if __name__ == '__main__':
+    cnt = 0
+    while True:
+        o, w = map(int, input().split())
+        if o == 0 and w == 0:   # "0 0" 입력 시 모든 시나리오 끝남
+            quit()
+
+        die = False
+        while True:
+            action, n = input().split()
+            if action == '#' and n == '0':
+                break
+
+            if not die:
+                n = int(n)
+                if action == 'E':
+                    w -= int(n)
+                elif action == 'F':
+                    w += int(n)
+
+            if w <= 0:  # 사망
+                die = True
+
+        cnt += 1
+        if w <= 0:
+            print("%d RIP" % cnt)
+        elif o / 2 < w < o * 2:
+            print("%d :-)" % cnt)
+        else:
+            print("%d :-(" % cnt)
+
 # 
