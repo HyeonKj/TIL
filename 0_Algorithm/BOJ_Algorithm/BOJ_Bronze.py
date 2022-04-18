886003ba163973caf4c01d60ab5986f9f0d3b424
HyeonKj <8.8.naine@gmail.com>
2022-04-18 15:51:21 +0900

    algorithm_bronze_1371

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1332,4 +1332,15 @@ if __name__ == '__main__':
         else:
             print("%d :-(" % cnt)
 
-# 
+# 1371
+import sys
+
+s = sys.stdin.read()
+li = [0]*26
+for c in s:
+    if c.islower():
+        li[ord(c)-97] += 1
+for i in range(26):
+    if li[i] == max(li):
+        print(chr(97+i), end='')
+
