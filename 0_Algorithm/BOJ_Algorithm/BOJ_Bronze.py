22af2e942d19d8acb827c034b3f96f19c707790a
HyeonKj <8.8.naine@gmail.com>
2022-04-19 17:36:02 +0900

    algorithm_bronze_1418

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1388,3 +1388,19 @@ m = (t%3600)//60
 s = t%60
 print("%02d:%02d:%02d" % (h,m,s))
 
+# 1418
+import sys
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+s = [0 for i in range(n+1)]
+for i in range(2,n+1):
+    if s[i] == 0:
+        for t in range(i,n+1,i):
+            if t%i == 0:
+                s[t] = max(s[t],i)
+ans = 0
+for i in s:
+    if i <= m:
+        ans += 1
+print(ans-1)
