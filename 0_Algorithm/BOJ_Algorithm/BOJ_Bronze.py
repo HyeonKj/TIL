30ed6f19cf0ee076842f8957bd2b0ce50e48ba7a
HyeonKj <8.8.naine@gmail.com>
2022-04-19 17:38:10 +0900

    algorithm_bronze_1453

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1430,3 +1430,18 @@ elif over>20:print("You are speeding and your fine is $270.")
 elif over>0:print("You are speeding and your fine is $100.")
 else:print("Congratulations, you are within the speed limit!")
 
+# 1453
+N = int(input())
+PC_Georgia = list(map(int, input().split()))        
+PC_checkmate = [0] * 101 #PC방 자리
+PC_refused = 0 #거절당한 사람
+
+for i in PC_Georgia:
+    if PC_checkmate[i] != 0:
+        PC_refused += 1
+    else:
+        PC_checkmate[i] += 1
+
+print(PC_refused)
+
+# 
