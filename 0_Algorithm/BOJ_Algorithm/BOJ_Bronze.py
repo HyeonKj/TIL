1e1def93ce9490e89caf908ee9a1a31aa80fe796
HyeonKj <8.8.naine@gmail.com>
2022-04-19 17:37:33 +0900

    algorithm_bronze_1440

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1420,3 +1420,13 @@ while i < N and j < M:
         j += 1
 print(sum(box)-in_box)    
 
+# 1440
+a=int(input())
+b=int(input())
+over=b-a
+ 
+if over >=31:print("You are speeding and your fine is $500.")
+elif over>20:print("You are speeding and your fine is $270.")
+elif over>0:print("You are speeding and your fine is $100.")
+else:print("Congratulations, you are within the speed limit!")
+
