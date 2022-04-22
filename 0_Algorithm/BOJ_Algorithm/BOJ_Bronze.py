7cf624992d6581543105f25a4fc28df2ce340160
HyeonKj <8.8.naine@gmail.com>
2022-04-23 01:38:52 +0900

    algorithm_b_1668

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1543,3 +1543,19 @@ for i in range(n):
     cnt_h = 0
 print(position_w, position_h)
 
+# 1668
+li = [int(input()) for _ in range(N)]
+left_cnt = right_cnt = 0
+left_max = right_max = 0
+for n in li:
+    if n > left_max:
+        left_max = n
+        left_cnt += 1
+for n in li[::-1]:
+    if n > right_max:
+        right_max = n
+        right_cnt += 1
+print(left_cnt)
+print(right_cnt)
+
+# 
