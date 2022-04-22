8b4d306fa66522c9e77d0d316fa87d40906b7d4d
HyeonKj <8.8.naine@gmail.com>
2022-04-23 01:38:12 +0900

    algorithm_b_1652

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1509,3 +1509,37 @@ y1 = (a-1)%4
 x2 = (b-1)//4 + 1 
 y2 = (b-1)%4
 print(abs(x2-x1) + abs(y2-y1))
+
+# 1652
+n = int(input())
+s = []
+cnt_w = 0
+cnt_h = 0
+position_w = 0
+position_h = 0
+for i in range(n):
+    s.append(input())
+for i in s:
+    for j in i:
+        if j == '.':
+            cnt_w += 1
+        else:
+            if cnt_w > 1:
+                position_w += 1
+            cnt_w = 0
+    if cnt_w > 1:
+        position_w += 1
+    cnt_w = 0
+for i in range(n):
+    for j in range(n):
+        if s[j][i] == '.':
+            cnt_h += 1
+        else:
+            if cnt_h > 1:
+                position_h += 1
+            cnt_h = 0
+    if cnt_h > 1:
+        position_h += 1
+    cnt_h = 0
+print(position_w, position_h)
+
