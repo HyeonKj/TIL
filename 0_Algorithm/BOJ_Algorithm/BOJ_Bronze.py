0e0526c58520facaaf20adb2ee60bb58c365a6af
HyeonKj <8.8.naine@gmail.com>
2022-04-24 21:10:02 +0900

    every do to

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1574,3 +1574,45 @@ for _ in range(n-1):
         
     b = itp[a+b]
 print(b)
+
+#1673
+while 1:
+    try:
+        n, k = map(int, input().split())
+        res = 0 
+        res += n
+        while n//k:
+            res += n//k
+            n = n//k + n%k
+        print(res)
+    except:
+        break
+
+# 1681
+N,l = list(map(int,input().split(' ')))
+
+num = 1 # 양의 정수이기 때문에 0이 아닌 1부터 시작합니다
+cnt = 0	# 라벨을 붙인 개수
+while True:
+    if str(l) not in str(num): #해당 숫자를 사용할 수 있으면
+        cnt+=1	#라벨을 붙입니다
+        if cnt == N: #N개의 원소를 다 라벨링했다면
+            print(num) #마지막 값을 출력하고
+            break #반복문을 그만둡니다
+        num+=1	#반복문이 끝나지 않았다면 다음 숫자로 넘어갑니다
+
+    else:	#l이 포함되어 해당 숫자를 사용할 수 없다면
+        num+=1	#다음 숫자로 넘어갑니다
+
+#1703
+while 1:
+    li = list(map(int, input().split()))
+    if li[0] == 0:
+        break
+    n = 1
+    for i in range(li[0]):
+        sf = li[2*i + 1]
+        p = li[2*i + 2]
+        n = n*sf - p
+    print(n)
+
