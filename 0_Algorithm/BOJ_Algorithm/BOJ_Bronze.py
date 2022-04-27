4f2dd619ffd423142e80813518aa607e807cedcb
HyeonKj <8.8.naine@gmail.com>
2022-04-28 00:05:05 +0900

    algorithm_b_1977

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1872,3 +1872,18 @@ for _ in range(int(input())):
     x = int(input())
     print(c[x])
 
+# 1977
+m = int(input())
+n = int(input())
+num = []
+i = 1
+while i ** 2 <= n:
+    if m <= i ** 2 <= n:
+        num.append(i ** 2)
+    i += 1
+if num == []:
+    print(-1)
+else:
+    print(sum(num))
+    print(num[0])
+
