e6ed4ea74b84f45e606d7d177ba20bda6634c1f5
HyeonKj <8.8.naine@gmail.com>
2022-04-28 00:03:12 +0900

    algorithm_b_1924

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1758,3 +1758,27 @@ for i in range(0, 26):
         count += abs(a[i] - b[i])
 print(count)
 
+# 1924 2007년
+Day = 0
+arrList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekList = ["SUN", "MON","TUE", "WED", "THU", "FRI", "SAT"]
+ 
+x, y = map(int,input().split())
+ 
+for i in range(x-1):
+    Day = Day + arrList[i]
+Day = (Day + y) % 7
+ 
+print(weekList[Day])
+ 
+'''
+# calendar module 사용
+import calendar
+
+arrList = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+x, y = map(int,input().split())
+
+Day = calendar.weekday(2007, x, y)
+print(arrList[Day])
+'''
+
