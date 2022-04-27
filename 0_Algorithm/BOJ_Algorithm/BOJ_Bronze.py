f191757d956294841165922076d212d19b417c1c
HyeonKj <8.8.naine@gmail.com>
2022-04-28 00:02:49 +0900

    algorithm_b_1919

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1741,3 +1741,20 @@ for _ in range(int(input())):
         s += (ord(L[i])-65) * 26**(2-i)
     print("nice" if abs(s-n) <= 100 else "not nice")
 
+# 1919
+a = [0 for i in range(26)]
+b = [0 for i in range(26)]
+count = 0
+a_gram = input()
+b_gram = input()
+a_gram = list(a_gram)
+b_gram = list(b_gram)
+for i in range(0, len(a_gram)):
+    a[ord(a_gram[i]) - 97] += 1
+for i in range(0, len(b_gram)):
+    b[ord(b_gram[i]) - 97] += 1
+for i in range(0, 26):
+    if a[i] != b[i]:
+        count += abs(a[i] - b[i])
+print(count)
+
