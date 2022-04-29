64215bcbc10ecf8540ffcea903300831ad35897f
HyeonKj <8.8.naine@gmail.com>
2022-04-30 01:34:58 +0900

    algorithm_b_2037

--- 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Algorithm/BOJ_Bronze.py
@@ -1895,3 +1895,27 @@ s = 0
 for _ in range(n):
     s += int(sys.stdin.readline())
 print(s-n+1)
+
+# 2037
+if __name__ == '__main__':
+    keyboard = [[], ['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'], ['M', 'N', 'O'],
+                ['P', 'Q', 'R', 'S'], ['T', 'U', 'V'], ['W', 'X', 'Y', 'Z']]
+    p, w = map(int, input().split())
+    text = input()
+
+    total = 0
+    past_j = ''
+    for i in range(len(text)):
+        if text[i] == ' ':
+            total += p
+            past_j = ''
+        for j in range(len(keyboard)):
+            for k in range(len(keyboard[j])):
+                if text[i] == keyboard[j][k]:
+                    if j == past_j:
+                        total += w
+                    total += (1+k) * p
+                    past_j = j
+    print(total)
+
+#
