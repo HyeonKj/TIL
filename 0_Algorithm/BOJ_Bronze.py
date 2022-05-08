963da230c1d2db2622d41bc2331f9930eafcd327
HyeonKj <8.8.naine@gmail.com>
2022-05-08 22:31:27 +0900

    Algorithm_bronze_2153

--- 0_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Bronze.py
@@ -1970,3 +1970,19 @@ while(True):
         # map은 이같은 배열 안의 스트링을 int형으로 편하게 변환시켜 주는 기능을 한다.
     print(n)
 
+#2153
+pn = [0 for i in range(1050)]
+for i in range(2, 32):
+    for j in range(i * 2, 1050, i):
+        pn[j] = 1
+n = input()
+word = 0
+for i in n:
+    if i.islower() == True:
+        word += ord(i) - 96
+    else:
+        word += ord(i) - 38
+if pn[word] == 0:
+    print('It is a prime word.')
+else:
+    print('It is not a prime word.')
