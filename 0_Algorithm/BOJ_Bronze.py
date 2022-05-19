c0556a64bf283f78e162462c68a3c3cc6fe70378
HyeonKj <8.8.naine@gmail.com>
2022-05-19 16:10:59 +0900

    algorithm_bronze_2309

--- 0_Algorithm/BOJ_Bronze.py
+++ 0_Algorithm/BOJ_Bronze.py
@@ -2061,3 +2061,21 @@ while n > nums_pileup :
     cnt += 1  # 반복문을 반복하는 횟수
 print(cnt)
 
+# 2309 일곱난쟁이
+s = []
+for i in range(9):
+    s.append(int(input()))
+sum_s = sum(s)
+one = 0
+two = 0
+for i in range(8):
+    for j in range(i + 1, 9):
+        if sum_s - (s[i] + s[j]) == 100:
+            one = s[i]
+            two = s[j]
+s.remove(one)
+s.remove(two)
+s.sort()
+for i in s:
+    print(i)
+    
